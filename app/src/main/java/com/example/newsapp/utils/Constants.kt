e3cffae3ce0a23a42befce47804019b03c1347bc
Abhas Kumar <abhas1102@gmail.com>
2021-09-19 02:43:59 +0530

    Set up Retrofit to get results from 3rd party News Api

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY = "a380d7f708d84f728fcccacccf0a8fb6"
+        const val BASE_URL = "https:newsapi.org"
+    }
+}
